0b5bbd18c6640b75f9a7e5a45824ecc66e30260f
Ryan <ryanjayanthonytimagos@gmail.com>
2025-04-11 00:12:20 +0800

    [Server] Removed URL

--- server/server.js
+++ server/server.js
@@ -11,10 +11,22 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['https://localhost:5173', 'https://som-ni-a.vercel.app']
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+const allowedOrigins = ['http://localhost:5173', 'https://som-ni-a.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 //API Endpoints
 app.get('/',(req,res)=> res.send("API Working"));
