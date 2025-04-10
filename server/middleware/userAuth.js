efb828144d15f36d5a71a763b711e5e72225a248
Ryan <ryanjayanthonytimagos@gmail.com>
2025-04-11 00:19:09 +0800

    [Server] Adjusted JSON

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,28 +1,26 @@
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
-    const {token} = req.cookies;
+  const { token } = req.cookies;
 
-    if(!token){
-        res.json({success:false,message: "Not Authorized. Login Again."});
+  if (!token) {
+    return res.status(401).json({ success: false, message: "Not Authorized. Login Again." });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.body = req.body || {};
+      req.body.userId = tokenDecode.id;
+      next();
+    } else {
+      return res.status(401).json({ success: false, message: "Not Authorized. Login Again" });
     }
 
-    try {
-        const tokenDecode = jwt.verify(token,process.env.JWT_SECRET)
-    
-        if(tokenDecode.id){
-            req.body = req.body || {};
-            req.body.userId = tokenDecode.id
-            next();
-        }else{
-            return res.json({success:false, message: "Not Authorized. Login Again"});
-        }
-
-       
-
-    } catch (error) {
-        res.json({sucess:false, message: error.message});
-    }
+  } catch (error) {
+    return res.status(401).json({ success: false, message: error.message });
+  }
 };
 
 export default userAuth;
