dee64feade17733f62a9f2fc160c7839d0965666
Ryan <ryanjayanthonytimagos@gmail.com>
2025-04-11 00:34:57 +0800

    [Server] Fixed CORS

--- server/server.js
+++ server/server.js
@@ -11,22 +11,10 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-
+const allowedOrigins = ['', 'https://som-ni-a.vercel.app']
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = ['http://localhost:5173', 'https://som-ni-a.vercel.app'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-
+app.use(cors({origin: allowedOrigins, credentials: true}));
 
 //API Endpoints
 app.get('/',(req,res)=> res.send("API Working"));
