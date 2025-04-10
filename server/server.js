2a24b2e4f9327138782dd3e81f8e9161407cba44
Ryan <ryanjayanthonytimagos@gmail.com>
2025-04-10 20:40:25 +0800

    Test Push

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://somnia-production.up.railway.app']
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
