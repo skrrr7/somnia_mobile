16f2dfa5200f0aff7b18e3cee204c87d0f2f4dbc
Ryan <ryanjayanthonytimagos@gmail.com>
2025-04-11 00:22:30 +0800

    [Server] Console Testing

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
+  console.log("Cookies received:", req.cookies); 
   const { token } = req.cookies;
 
   if (!token) {
