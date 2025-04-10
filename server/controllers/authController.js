b0ff339d25cfe83ea65d4f8b7346bff05283ab0e
Ryan <ryanjayanthonytimagos@gmail.com>
2025-04-11 00:50:09 +0800

    [Client] Added Favicon logo

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,7 +29,14 @@ export const register = async (req, res) => {
             sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
-
+        /*
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        */
         const mailOptions = {
             from: process.env.SENDER_EMAIL,
             to: email,
@@ -77,7 +84,14 @@ export const login = async (req, res) => {
             sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
-
+        /*
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        */
         return res.status(200).json({ success: true, message: "Login successful" });
 
     } catch (error) {
@@ -93,7 +107,13 @@ export const logout = async (req, res) => {
             secure: true,
             sameSite: 'None'
         });
-
+        /*
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Strict',
+        });
+        */
         return res.status(200).json({ success: true, message: 'Logged Out!' });
 
     } catch (error) {
