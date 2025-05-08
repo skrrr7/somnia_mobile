0f0b4a42e77be3ca55c8390bacd8318a58842eda
Ryan <ryanjayanthonytimagos@gmail.com>
2025-05-08 14:46:43 +0800

    [Server] Added allowedOrigins

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173', 'https://som-ni-a.vercel.app']
+const allowedOrigins = [
+    'http://localhost:5173',         // Vite (web) frontend
+    'https://som-ni-a.vercel.app',  // Deployed frontend
+    'http://localhost:8081',         // Expo Go default local web (Expo dev tools)
+    'http://localhost:19006',        // Expo web
+    'exp://127.0.0.1:19000',         // Expo mobile local dev (iOS/Android)
+    'http://192.168.0.X:19006',      // Replace with your LAN IP if using physical device
+  ];
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
