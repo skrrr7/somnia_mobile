0e87442ecc8ffe256b2be26b89e99f1b7c0c9ad0
Ryan <ryanjayanthonytimagos@gmail.com>
2025-05-08 13:37:59 +0800

    [Client] Dashboard Draft

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,43 +1,39 @@
 import React, {useState} from 'react'
 import NavigationBar from '../components/NavigationBar'
-import background from '../assets/background.jpeg';
+import HealthOverview from '../components/HealthOverview'
 const Dashboard = () => {
 
-  const [activeSection, setActiveSection] = useState("Take Test");
-
+  const[activation, setActivation] = useState("Health Overview")
   return (
     
-    <div className="min-h-screen flex items-center justify-center p-8"
-     style={{
-       backgroundImage: `url(${background})`,
-       backgroundSize: 'cover',
-       backgroundPosition: 'center',
-       backgroundAttachment: 'fixed',
-     }}>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <NavigationBar/>
-    <div className= "w-200 ml-10 h-150 mt-25 rounded-2xl p-6  bg-indigo-950/50 shadow-[-20px_0_25px_5px_rgba(255,255,255,0.5)]">
-        <div onClick={() => setActiveSection("Take Test")} className="border-b border-white py-3 px-2 hover:bg-white/10 hover:rounded-md mb-5
-        cursor-pointer transition duration-200 text-white font-bold tracking-wide font-sans text-center">Take Test</div>
-        <div onClick={() => setActiveSection("Vitals")} className="border-b border-white py-3 px-2 hover:bg-white/10 hover:rounded-md mb-5
-        cursor-pointer transition duration-200 text-white font-bold tracking-wide font-sans text-center">Vitals</div>
-        <div onClick={() => setActiveSection("Statistics")} className="border-b border-white py-3 px-2 hover:bg-white/10 hover:rounded-md mb-5
-        cursor-pointer transition duration-200 text-white font-bold tracking-wide font-sans text-center">Statistics</div>
-        <div onClick={() => setActiveSection("Sleep History")} className="border-b border-white py-3 px-2 hover:bg-white/10 hover:rounded-md mb-5
-        cursor-pointer transition duration-200 text-white font-bold tracking-wide font-sans text-center">Sleep History</div>
-        <div onClick={() => setActiveSection("Sleep Recommendations")} className="border-b border-white py-3 px-2 hover:bg-white/10 hover:rounded-md mb-5
-        cursor-pointer transition duration-200 text-white font-bold tracking-wide font-sans text-center">Sleep Recommendations</div>
-        <div onClick={() => setActiveSection("AI Prediction")} className="border-b border-white py-3 px-2 hover:bg-white/10 hover:rounded-md mb-5
-        cursor-pointer transition duration-200 text-white font-bold tracking-wide font-sans text-center">AI Prediction</div>
-        
+    <div className= "w-200 mr-2 ml-10 min-h-160 mt-15 p-6 bg-slate-950 border border-black">
+
+      <div className="w-30 h-30 rounded-full bg-white mx-auto p-1 mb-1"></div>
+      <div className="text-lg font-medium text-center text-white p-1 mb-1">John Doe</div>
+      <div className="text-green-400 text-sm mx-auto w-fit p-1 mb-5">Verified</div>
+
+      <div onClick={() => setActivation("Health Overview")} 
+      className='text-center border border-3 border-slate-800 text-white p-5 mb-5'>Health Overview</div>
+
+      <div onClick={() => setActivation("AI Prediction")} 
+      className='text-center border border-3 border-slate-800 text-white p-5 mb-5'>AI Prediction</div>
+
+      <div onClick={() => setActivation("Sleep History")} 
+      className='text-center border border-3 border-slate-800 text-white p-5 mb-5'>Sleep History</div>
+
+      <div onClick={() => setActivation("Statistics")} 
+      className='text-center border border-3 border-slate-800 text-white p-5 mb-5'>Statistics</div>
+
     </div>
-    <div className="w-900 h-150 ml-10 mt-25 rounded-2xl p-6 shadow-2xl bg-indigo-950/50 shadow-[20px_0_25px_5px_rgba(255,255,255,0.5)]">
-        {activeSection === "Take Test" && <div className="text-white"> Take Test Content</div>}
-        {activeSection === "Vitals" && <div className="text-white"> Vitals Content</div>}
-        {activeSection === "Statistics" && <div className="text-white"> Statistics Content</div>}
-        {activeSection === "Sleep History" && <div className="text-white"> Sleep History Content</div>}
-        {activeSection === "Sleep Recommendations" && <div className="text-white"> Sleep Recommendations Content</div>}
-        {activeSection === "AI Prediction" && <div className="text-white"> AI Prediction Content</div>}       
-  </div>
+
+    <div className= "w-900 mr-5 ml-2 h-160 mt-15 p-6 bg-slate-950 border border-black overflow-y-auto">
+      {activation === "Health Overview" && (<HealthOverview/>)}
+
+    </div>
+
+    
 </div>
 
   )
