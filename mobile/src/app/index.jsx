5b539dd534be9107f2baf0389241f2e1f18b596d
ellydhore <ellydhoregabrylle.belleza@gmail.com>
2025-05-17 15:16:32 +0800

    Created a new expo app with a blank template

---
+++ mobile/src/app/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+    
+      <Text>Steps Count</Text>
+      <Text>0</Text>
+
+      <Text>{"\n"}Latest Heart Rate</Text>
+      <Text>0 bmp</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
